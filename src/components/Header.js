51b251043201d9ffe7d37c4a3e89257f837cf82b
ramiinberg <rami.inberg@gmail.com>
2022-07-14 10:31:56 +0300

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import trollFaceImg from '../images/troll-face.png'
+import './header.css'
+
+const Header = () => (
+  <header className="header">
+    <div className='header-logo'>
+      <img src={trollFaceImg} alt='troll face' className='header-image' />
+      <h2>Meme Generator</h2>
+    </div>
+    <h4 className='header-links'>React practising</h4>
+  </header>
+)
+
+export default Header
