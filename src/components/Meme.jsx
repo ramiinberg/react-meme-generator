aeb0c82b83c265b07cfc1044fb21c939bc4baba9
ramiinberg <rami.inberg@gmail.com>
2022-07-15 14:56:39 +0300

    showing fetched meme image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,26 +1,36 @@
+import React from 'react'
 import memesData from '../memesData'
 import './meme.css'
 
 function Meme() {
+  const [memeImage, setMemeImage] = React.useState('')
+
   const getRandomImage = () => {
     const memesArray = memesData.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
     const imageUrl = memesArray[randomNumber].url
-    console.log(imageUrl)
+    setMemeImage(imageUrl)
   }
 
   return (
-    <div className="meme">
-      <input type="text" placeholder="Top text" className="meme-top-input" />
-      <input
-        type="text"
-        placeholder="Bottom text"
-        className="meme-bottom-input"
-      />
-      <button type="button" className="meme-new-image" onClick={getRandomImage}>
-        Get a new meme image 🖼
-      </button>
-    </div>
+    <>
+      <div className='meme'>
+        <input type='text' placeholder='Top text' className='meme-top-input' />
+        <input
+          type='text'
+          placeholder='Bottom text'
+          className='meme-bottom-input'
+        />
+        <button
+          type='button'
+          className='meme-new-image'
+          onClick={getRandomImage}
+        >
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img className='meme-image' src={memeImage} alt='Random meme' />
+    </>
   )
 }
 
